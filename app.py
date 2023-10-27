de579f966568b38d9c05769c49e83d86474f9acb
jieyuan <jieyuan1@ualberta.ca>
2023-10-27 12:31:09 -0700

    added security header and some minor change

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required, current_user
 import uuid
+import logging
 
 # Configure application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
 app.config['SECURITY_PASSWORD_SALT'] = 'your_password_salt_here'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+app.config['PERMANENT_SESSION_LIFETIME'] = 3600  # timeout after 1 hour
 db = SQLAlchemy(app)
+logging.basicConfig(filename='app.log', level=logging.INFO) # logs
 
 # Define models
 roles_users = db.Table('roles_users',
@@ -35,15 +41,26 @@ class User(db.Model, UserMixin):
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
+# Security headers
+@app.after_request
+def apply_security_headers(response):
+    response.headers["X-Content-Type-Options"] = "nosniff" # prevents MIME type sniffing
+    response.headers["X-Frame-Options"] = "SAMEORIGIN" # prevents clickjacking
+    response.headers["X-XSS-Protection"] = "1; mode=block" #protect against XSS attacks
+    response.headers["Content-Security-Policy"] = "default-src 'self'" # restricts the sources from which content can be loaded
+    return response
+
 @app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
     if User.query.filter_by(username=username).first():
+        logging.warning(f'Registration attempt with existing username: {username}')
         return jsonify(error='Username already exists'), 400
     user_datastore.create_user(username=username, password=password)
     db.session.commit()
+    logging.info(f'User registered: {username}')
     return '', 201
 
 @app.route('/login', methods=['POST'])
@@ -54,7 +71,9 @@ def login():
     user = User.query.filter_by(username=username).first()
     if user and user_datastore.verify_password(password, user.password):
         user_datastore.commit()
+        logging.info(f'User logged in: {username}')
         return '', 200
+    logging.warning(f'Invalid login attempt: {username}')
     return jsonify(error='Invalid credentials'), 401
 
 @app.route('/changepw', methods=['POST'])
@@ -66,19 +85,24 @@ def changepw():
     if user_datastore.verify_password(old_password, current_user.password):
         current_user.password = user_datastore.hash_password(new_password)
         db.session.commit()
+        logging.info(f'Password changed for user: {current_user.username}')
         return '', 201
+    logging.warning(f'Incorrect password change attempt by user: {current_user.username}')
     return jsonify(error='Incorrect old password'), 400
 
 @app.route('/admin', methods=['GET'])
 @login_required
 def admin():
     if current_user.has_role('admin'):
+        logging.info(f'Admin access by user: {current_user.username}')
         return f'Logged in as admin {current_user.username}'
+    logging.warning(f'Unauthorized admin access attempt by user: {current_user.username}')
     return jsonify(error='Unauthorized'), 403
 
 @app.route('/user', methods=['GET'])
 @login_required
 def user():
+    logging.info(f'User access: {current_user.username}')
     return f'Logged in as user {current_user.username}'
 
 if __name__ == '__main__':
@@ -86,7 +110,9 @@ if __name__ == '__main__':
         db.create_all()
         if not Role.query.filter_by(name='admin').first():
             user_datastore.create_role(name='admin')
+        if not Role.query.filter_by(name='user').first():
+            user_datastore.create_role(name='user')
         if not User.query.filter_by(username='admin').first():
-            user_datastore.create_user(username='admin', password='password', roles=['admin'])
+            user_datastore.create_user(username='admin', password='admin123', roles=['admin'])
             db.session.commit()
     app.run(ssl_context='adhoc')
